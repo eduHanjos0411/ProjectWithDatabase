09803bb49706f782b5cd7eec2413f99d4d09cdb9
Eduardo Henrique <eduardo.anjos@gestao.gov.br>
2025-01-09 11:32:08 -0300

    delete impl and dpt dao creation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 
 		SellerDao SellerDao = DaoFactory.createSellerDao();
 
@@ -31,11 +34,23 @@ public class Program {
 			System.out.println(obj);
 
 		}
-		
+		/*
 		System.out.println("=== TEST 4: seller insert ====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		SellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		*/
 
+		System.out.println("=== TEST 5: seller update ====");
+		seller = SellerDao.findById(2);
+		seller.setName("Martha Wayne");
+		SellerDao.update(seller);
+		System.out.println("Update completed");
+		
+		System.out.println("=== TEST 6: seller delete ====");
+		System.out.println("Enter id for delete test");
+		int id  = sc.nextInt();
+		SellerDao.deleteById(id);
+		System.out.println("id deleted with sucess");
 	}
 }
